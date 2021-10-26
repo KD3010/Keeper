b91d67e99002a214928fc60c7455ff8aeb6142af
Kushagra Devda <devdakushagra@gmail.com>
2021-10-26 18:10:47 +0530

    Adding App.jsx, Header.js, Footer.jsx and Note.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const date = new Date();
+const year = date.getUTCFullYear()
+
+function Footer(){
+    return <footer>
+        <p>Copyright© {year}</p>
+    </footer>
+}
+
+export default Footer;
---
+++ src/Components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h3>Title</h3>
+        <p>This is the content of the note.</p>
+    </div>
+}
+
+export default Note;
