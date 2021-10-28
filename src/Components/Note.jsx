e8672f8015e8687ed0a61f927bd0697de10c1591
Kushagra Devda <devdakushagra@gmail.com>
2021-10-28 16:14:55 +0530

    Adding map functionality and React props to the app.

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return <div className="note">
-        <h3>Title</h3>
-        <p>This is the content of the note.</p>
+        
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
     </div>
 }
 
